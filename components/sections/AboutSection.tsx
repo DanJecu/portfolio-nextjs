e39861974304f70c7042b01f4e5238db73e074a9
Dan Jecu <jdanalexandru@gmail.com>
2023-04-26 21:02:40 +0200

    escape '

--- components/sections/AboutSection.tsx
+++ components/sections/AboutSection.tsx
@@ -9,7 +9,7 @@ export const AboutSection: React.FC = () => {
             </div>
             <div className='pt-5 w-3/4 h-44'>
                 <Image
-                    className='m-auto blur-[1px]  contrast-125 brightness-50 drop-shadow-md  rounded transform transition duration-500 hover:scale-x-[-1]'
+                    className='m-auto blur-  contrast-125 brightness-50 drop-shadow-md  rounded transform transition duration-500 hover:scale-x-[-1]'
                     src='/profile.png'
                     width='150'
                     height='150'
@@ -22,11 +22,11 @@ export const AboutSection: React.FC = () => {
                 />
             </div>
             <p className='w-[85%] pt-12 leading-relaxed text-justify text-[#A9AEC0]'>
-                Deeply invested in the life-long learning mentality, I'm a
+                Deeply invested in the life-long learning mentality, I`m a
                 full-stack developer who carries a keen eye for design in an
                 user-first approach. My focus is in creating responsive layouts
                 on the JavaScript stack of React and NodeJS, and from my
-                background as a data analyst, I'm well acquainted to bringing
+                background as a data analyst, I`m well acquainted to bringing
                 out solutions to complex problems.
             </p>
         </section>
