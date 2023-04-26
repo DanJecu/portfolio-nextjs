00c11b30138b3d1eeb4cb91860a4eb135aa9868b
Dan Jecu <jdanalexandru@gmail.com>
2023-04-26 07:11:15 +0200

    Finished HeroSection

---
+++ components/Logo.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+
+export const Logo: React.FC = () => {
+    const [showText, setShowText] = useState(false);
+    useEffect(() => {
+        if (!showText) setShowText(true);
+    }, [showText]);
+    return (
+        <div>
+            <h1 className='text-center pb-1 text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
+                <motion.span
+                    layout
+                    key='d'
+                    style={{ display: 'inline-block' }}
+                    transition={{ ease: 'linear', duration: 0.7 }}
+                >
+                    D
+                </motion.span>
+                {showText && (
+                    <motion.span
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        style={{ display: 'inline-block' }}
+                        transition={{ delay: 0.8, ease: 'easeIn', duration: 2 }}
+                    >
+                        AN
+                    </motion.span>
+                )}
+                {showText && <span> </span>}
+                <motion.span
+                    layout
+                    key='j'
+                    style={{ display: 'inline-block' }}
+                    transition={{ ease: 'linear', duration: 0.7 }}
+                >
+                    J
+                </motion.span>
+                {showText && (
+                    <motion.span
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        style={{ display: 'inline-block' }}
+                        transition={{ delay: 0.8, ease: 'easeIn', duration: 2 }}
+                    >
+                        ECU
+                    </motion.span>
+                )}
+            </h1>
+            <motion.h2
+                className='pt-1 text-2xl text-center text-[#a9aec0] font-thin md:text-3xl lg:text-4xl xl:text-5xl'
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }}
+                transition={{ delay: 2, ease: 'linear', duration: 0.7 }}
+            >
+                Full Stack Developer
+            </motion.h2>
+        </div>
+    );
+};
--- components/sections/HeroSection.tsx
+++ components/sections/HeroSection.tsx
@@ -1,15 +1,12 @@
 import Image from 'next/image';
-import { Text } from '../Text';
-
-import { motion, useAnimate } from 'framer-motion';
+import { Logo } from '../Logo';
+import { motion } from 'framer-motion';
 
 export const HeroSection: React.FC = () => {
-    const [scope, animate] = useAnimate();
-
     return (
-        <section className='flex flex-col justify-center items-center w-[90%] m-auto text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>
+        <section className='flex flex-col justify-center items-center w-[90%] m-auto'>
             <div className='flex my-auto text-white font-bold '>
-                <Text />
+                <Logo />
             </div>
             <Image
                 alt='down-arrow'
