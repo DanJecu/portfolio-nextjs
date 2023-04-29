40b353f085a770a8057aa26a8b1e32d0e77e2264
Dan Jecu <jdanalexandru@gmail.com>
2023-04-29 21:34:54 +0200

    done tablet view

--- components/sections/AboutSection.tsx
+++ components/sections/AboutSection.tsx
@@ -1,24 +1,39 @@
+import { useRef } from 'react';
 import Image from 'next/image';
 import profilePic from '../../public/profile.png';
 
+import { useInView } from 'framer-motion';
+
 export const AboutSection: React.FC = () => {
+    const ref = useRef(null);
+    const isInView = useInView(ref);
+
+    const style = {
+        transform: isInView ? 'none' : 'translateX(200px)',
+        opacity: isInView ? 1 : 0,
+        transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
+    };
+
     return (
-        <section className='flex flex-col items-center text-white lg:w-[70%] sm:w-[90%] m-auto'>
-            <div className='flex items-center w-[95%] self-start mr-auto p-7 lg:w-[70%] lg:p-14'>
-                <h2 className=' font-bold text-2xl pr-4'>About</h2>
-                <div className='h-[1.5px] bg-white flex-1'></div>
+        <section
+            className='m-auto flex flex-col items-center text-white sm:w-[90%] lg:grid lg:w-[80%] lg:grid-cols-2 lg:grid-rows-[20%_80%] lg:justify-items-center'
+            style={style}
+        >
+            <div className='mr-auto flex w-[95%] items-center self-start p-7  lg:col-end-[-1] lg:w-[80%] lg:p-0 lg:pt-14'>
+                <h2 className='pr-4 text-2xl font-bold'>About</h2>
+                <div className='h-[1.5px] flex-1 bg-white'></div>
             </div>
-            <div className='relative'>
+            <div className='relative' ref={ref}>
                 <Image
                     key='1'
-                    className='m-auto rounded-3xl saturate-150 drop-shadow-md  mask-image-gradient md:w-[90%] md:h-[90%]'
+                    className='mask-image-gradient m-auto rounded-3xl drop-shadow-md  saturate-150 md:h-[90%] md:w-[90%]'
                     src={profilePic}
                     width='250'
                     height='250'
                     alt='profile picture'
                 />
             </div>
-            <p className='w-[85%] pt-10 md:pt-16 leading-relaxed text-[0.94rem] text-center text-[#A9AEC0] md:text-lg'>
+            <p className='w-[85%] pt-10 text-center text-[0.94rem] leading-relaxed text-[#A9AEC0] md:pt-16 md:text-lg lg:pt-0'>
                 Deeply invested in the life-long learning mentality, I&#39;m a
                 full-stack developer who carries a keen eye for design in an
                 user-first approach. My focus is in creating responsive layouts
