086e1e4b8f9639e75022be1daa9bd3d4dc7f5955
Dan Jecu <jdanalexandru@gmail.com>
2023-04-30 19:37:49 +0200

    fix bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ export default function Home() {
             <main>
                 <HeroSection />
                 <AboutSection />
-                <SkillsSection />
             </main>
         </>
     );
