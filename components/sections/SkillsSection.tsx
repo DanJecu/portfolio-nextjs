1b87339e8a23122c0c29f3c42341192d02097158
Dan Jecu <jdanalexandru@gmail.com>
2023-05-02 18:43:39 +0200

    added skills cards

--- components/sections/SkillsSection.tsx
+++ components/sections/SkillsSection.tsx
@@ -1,3 +1,11 @@
 export const SkillsSection: React.FC = () => {
-    return <section className=''>Sections Skills</section>;
+    return (
+        <section className='flex flex-col justify-between gap-4 p-8 text-white [&>div]:flex-grow [&>div]:bg-card [&>div]:p-2 [&>div]:text-center'>
+            <div className=''>
+                <h4>Frontend</h4>
+            </div>
+            <div className=''>Backend</div>
+            <div className=''>Miscellaneous</div>
+        </section>
+    );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,10 +13,11 @@ export default function Home() {
             </Head>
             <main className=' overflow-hidden'>
                 <nav className='fixed top-0 h-7 w-full bg-indigo-500 text-center text-lg text-white opacity-50'>
-                    ⚠️ This website is currently under construction 
+                    ⚠️ This website is currently under construction
                 </nav>
                 <HeroSection />
                 <AboutSection />
+                <SkillsSection />
             </main>
         </>
     );
