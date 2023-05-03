c523e6ebed9f9eba2abfd2bf4372561cdc8b9cea
Dan Jecu <jdanalexandru@gmail.com>
2023-05-03 22:28:02 +0200

    created cards for skils

--- components/sections/SkillsSection.tsx
+++ components/sections/SkillsSection.tsx
@@ -1,11 +1,22 @@
 export const SkillsSection: React.FC = () => {
     return (
-        <section className='flex flex-col justify-between gap-4 p-8 text-white [&>div]:flex-grow [&>div]:bg-card [&>div]:p-2 [&>div]:text-center'>
-            <div className=''>
-                <h4>Frontend</h4>
+        <section className='flex h-auto flex-col justify-between gap-4 p-8 text-white'>
+            <span className='flex w-[95%] items-center pb-4  lg:col-start-2  lg:mt-auto lg:w-[100%] lg:p-2'>
+                <h2 className='pr-4 text-2xl font-bold lg:text-xl'>Skills</h2>
+                <div className='h-[1.5px] flex-1 bg-white'></div>
+            </span>
+            <div className='pb-6 text-center'>
+                <h4 className='pb-1 text-xl'>Frontend</h4>
+                <div className='h-96 rounded-lg bg-card p-2'></div>
             </div>
-            <div className=''>Backend</div>
-            <div className=''>Miscellaneous</div>
+            <div className=' pb-6 text-center'>
+                <h4 className='pb-1 text-xl'>Backend</h4>
+                <div className='h-96 rounded-lg bg-card p-2'></div>
+            </div>
+            <div className='pb-6 text-center'>
+                <h4 className='pb-1 text-xl'>Miscellaneous</h4>
+                <div className='h-96 rounded-lg bg-card p-2'></div>
+            </div>
         </section>
     );
 };
