70dd25d6232997fa2f2d884b928d0fe1d05fa3b5
Dan Jecu <jdanalexandru@gmail.com>
2023-05-07 22:23:17 +0200

    created layout for laptop screen

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -7,9 +7,9 @@ interface SkillCardProps {
 
 export const SkillCard: React.FC<SkillCardProps> = ({ category, title }) => {
     return (
-        <div className='mx-auto h-1/3 w-[95%] pb-6 text-center drop-shadow-sm md:w-[70%] lg:h-4/5'>
+        <div className='mx-auto h-1/3 w-[95%] pb-6 text-center drop-shadow-sm md:w-[70%] lg:h-4/5 lg:w-full'>
             <h4 className='pb-1 text-xl opacity-90'>{title}</h4>
-            <div className='item-center flex h-96 flex-wrap items-center justify-center gap-5 rounded-lg bg-card px-6 pb-8 pt-4 text-secondary shadow drop-shadow-sm md:gap-12 md:pb-16 md:pt-8'>
+            <div className='item-center flex h-96 flex-wrap items-center justify-center gap-5 rounded-lg bg-card px-6 pb-8 pt-4 text-secondary shadow drop-shadow-sm md:gap-12 md:pb-16 md:pt-8 lg:h-[30rem] lg:w-72 lg:justify-around lg:gap-6 lg:p-6 lg:pb-8'>
                 <SkillIcons category={category} />
             </div>
         </div>
--- components/sections/SkillsSection.tsx
+++ components/sections/SkillsSection.tsx
@@ -2,13 +2,13 @@ import { frontend, backend, misc } from '@/utils/icons';
 import { SkillCard } from '../SkillCard';
 
 export const SkillsSection: React.FC = () => (
-    <section className='flex h-1/3 flex-col gap-4 p-2 text-white'>
-        <span className='flex w-[95%] items-center pb-4 lg:col-start-2 lg:w-[50%] lg:px-12 lg:pb-12'>
-            <h2 className='pr-4 text-2xl font-bold lg:text-xl'>Skills</h2>
+    <section className='m-auto flex h-1/3 w-[95%] flex-col gap-4 p-2 text-white'>
+        <span className='flex w-[95%] items-center p-7 lg:w-[50%] lg:px-12 lg:pb-12 lg:pt-24'>
+            <h2 className='pr-4 text-2xl font-bold'>Skills</h2>
             <div className='h-[1.5px] flex-1 bg-white'></div>
         </span>
         <div
-            className='mx-auto flex w-11/12 flex-col lg:h-4/5 lg:w-4/5 lg:flex-row lg:gap-10
+            className='m-auto flex w-11/12 flex-col lg:h-4/5 lg:w-[100%] lg:flex-row lg:gap-10
         '
         >
             <SkillCard category={frontend} title='Frontend' />
