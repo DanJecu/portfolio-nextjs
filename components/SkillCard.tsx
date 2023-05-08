a0c604109fcc8a319119f10d8b5a740476fd5131
Dan Jecu <jdanalexandru@gmail.com>
2023-05-08 07:22:59 +0200

    fixed alignment

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -7,7 +7,7 @@ interface SkillCardProps {
 
 export const SkillCard: React.FC<SkillCardProps> = ({ category, title }) => {
     return (
-        <div className='mx-auto h-1/3 w-[95%] pb-6 text-center drop-shadow-sm md:w-[70%] lg:h-4/5 lg:w-full'>
+        <div className='mx-auto flex h-1/3 w-[95%] flex-col items-center pb-6 text-center drop-shadow-sm md:w-[70%] lg:h-4/5 lg:w-full'>
             <h4 className='pb-1 text-xl opacity-90'>{title}</h4>
             <div className='item-center flex h-96 flex-wrap items-center justify-center gap-5 rounded-lg bg-card px-6 pb-8 pt-4 text-secondary shadow drop-shadow-sm md:gap-12 md:pb-16 md:pt-8 lg:h-[30rem] lg:w-72 lg:justify-around lg:gap-6 lg:p-6 lg:pb-8'>
                 <SkillIcons category={category} />
