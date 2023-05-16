6b633071730a34402ade63c52f4aedd01700b735
Dan Jecu <jdanalexandru@gmail.com>
2023-05-16 21:29:23 +0200

    added contact section title

---
+++ components/sections/ContactSection.tsx
@@ -0,0 +1,11 @@
+export const ContactSection: React.FC = () => {
+    return (
+        <section>
+            <div className='flex w-[95%] items-center justify-center p-7 text-white  lg:col-start-1 lg:col-end-3 lg:mt-auto lg:w-[50%]  lg:pt-24'>
+                <div className='h-[1.5px] flex-1 bg-white'></div>
+                <h2 className='px-4 text-2xl font-bold '>Contact</h2>
+                <div className='h-[1.5px] flex-1 bg-white'></div>
+            </div>
+        </section>
+    );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import Head from "next/head";
+import Head from 'next/head';
 
 // Sections
-import { HeroSection } from "@/components/sections/HeroSection";
-import { AboutSection } from "@/components/sections/AboutSection";
-import { SkillsSection } from "@/components/sections/SkillsSection";
-import { ProjectsSection } from "@/components/sections/ProjectsSection";
+import { HeroSection } from '@/components/sections/HeroSection';
+import { AboutSection } from '@/components/sections/AboutSection';
+import { SkillsSection } from '@/components/sections/SkillsSection';
+import { ProjectsSection } from '@/components/sections/ProjectsSection';
+import { ContactSection } from '@/components/sections/ContactSection';
 
 export default function Home() {
     return (
@@ -12,9 +13,9 @@ export default function Home() {
             <Head>
                 <title>Dan Jecu</title>
             </Head>
-            <main className="overflow-hidden">
-                <nav className="fixed top-0 flex h-5 w-full items-center justify-center bg-[#D08FC7] text-xs text-black opacity-70 lg:h-7 lg:text-lg">
-                    <h6 className="my-auto">
+            <main className='overflow-hidden'>
+                <nav className='fixed top-0 flex h-5 w-full items-center justify-center bg-[#D08FC7] text-xs text-black opacity-70 lg:h-7 lg:text-lg'>
+                    <h6 className='my-auto'>
                         ⚠️ This website is currently under construction
                     </h6>
                 </nav>
@@ -22,6 +23,7 @@ export default function Home() {
                 <AboutSection />
                 <SkillsSection />
                 <ProjectsSection />
+                <ContactSection />
             </main>
         </>
     );
