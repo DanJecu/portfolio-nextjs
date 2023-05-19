3ec657043476bfb925f0a01b4232880a25e59d0a
Dan Jecu <jdanalexandru@gmail.com>
2023-05-19 14:45:30 +0200

    fixed laptop screen

--- components/sections/ContactSection.tsx
+++ components/sections/ContactSection.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { Footer } from '../Footer';
 
 export const ContactSection: React.FC = () => {
     return (
-        <section className='flex flex-col  pt-20 text-white md:pt-2'>
-            <div className='flex w-[100%] items-center justify-center p-7 pb-12  lg:col-start-1 lg:col-end-3 lg:mt-auto lg:w-[50%]  lg:pt-24'>
+        <section className='flex flex-col  pt-20 text-white md:pt-2 lg:pt-0'>
+            <div className='flex w-[100%] items-center justify-center p-7 pb-12 '>
                 <div className='h-[1.5px] flex-1 bg-white'></div>
                 <h2 className='px-4 text-2xl font-bold '>Contact</h2>
                 <div className='h-[1.5px] flex-1 bg-white'></div>
@@ -12,7 +13,9 @@ export const ContactSection: React.FC = () => {
             <div className='my-auto flex flex-col items-center leading-relaxed'>
                 <h4 className='pb-1 text-secondary'>WRITE ME AN EMAIL</h4>
                 <h4 className='pb-6 text-xl font-bold tracking-wider'>
-                    danjecu@outlook.com
+                    <Link href='mailto:danjecu@outlook.com' target='_blank'>
+                        danjecu@outlook.com
+                    </Link>
                 </h4>
                 <h4 className='pb-6 tracking-[.2em] text-secondary'>OR</h4>
                 <h4 className='pb-4 text-secondary'>REACH ME THROUGH</h4>
