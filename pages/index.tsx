8a0ccfca569767f473f9ef2e0ee8cffa9b8f26d4
Dan Jecu <jdanalexandru@gmail.com>
2023-05-19 12:20:56 +0200

    removed navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,11 +14,6 @@ export default function Home() {
                 <title>Dan Jecu</title>
             </Head>
             <main className='overflow-hidden'>
-                <nav className='fixed top-0 flex h-5 w-full items-center justify-center bg-[#D08FC7] text-xs text-black opacity-70 lg:h-7 lg:text-lg'>
-                    <h6 className='my-auto'>
-                        ⚠️ This website is currently under construction
-                    </h6>
-                </nav>
                 <HeroSection />
                 <AboutSection />
                 <SkillsSection />
