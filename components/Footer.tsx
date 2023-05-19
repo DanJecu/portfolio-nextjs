aac9f5fdef4b88153851d29651a2ad8d2802f92e
Dan Jecu <jdanalexandru@gmail.com>
2023-05-19 14:39:15 +0200

    fix footer for tablet

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export const Footer: React.FC = () => {
     const currentYear: number = new Date().getFullYear();
     return (
-        <footer className='mx-auto flex  border-t-2 border-secondary p-4 text-secondary'>
+        <footer className='mx-auto flex  border-t-2 border-secondary p-4 text-secondary md:w-11/12'>
             <p className='mx-auto'>
                 Dan Jecu · {currentYear} · All rights reserved
             </p>
