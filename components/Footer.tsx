3b87ae78011d2c43e50232549552ee209dfd0bd3
Dan Jecu <jdanalexandru@gmail.com>
2023-05-19 12:14:07 +0200

    added footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,10 @@
 export const Footer: React.FC = () => {
-    return <footer>This is footer</footer>;
+    const currentYear: number = new Date().getFullYear();
+    return (
+        <footer className='mx-auto flex  border-t-2 border-secondary p-4 text-secondary'>
+            <p className='mx-auto'>
+                Dan Jecu · {currentYear} · All rights reserved
+            </p>
+        </footer>
+    );
 };
