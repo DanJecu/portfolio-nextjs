e5f0fa21700392d8e7ed3b5648961b8095986397
Dan Jecu <jdanalexandru@gmail.com>
2023-05-20 13:24:41 +0200

    adjusted size for projects

--- components/sections/ProjectsSection.tsx
+++ components/sections/ProjectsSection.tsx
@@ -24,20 +24,20 @@ export const ProjectsSection: React.FC = () => {
                 <div className='h-[1.5px] flex-1 bg-white'></div>
             </span>
             <div className='flex'>
-                <div
-                    className='relative m-auto hidden h-[80vh] w-[80%] rounded-lg md:block md:w-[38%] lg:h-[80%]'
-                    
-                >
+                <div className='relative m-auto hidden h-[80vh] w-[80%] rounded-lg md:w-[38%] lg:h-[80%] xl:block'>
                     <img
                         key='1'
-                        className='h-full w-full rounded-lg object-fill lg:h-[100%]'
+                        className='h-full w-full rounded-lg object-fill lg:h-[100%] '
                         src='./projects/mealShare2.png'
                         alt='profile picture'
                         width=''
                         height=''
                     />
                 </div>
-                <div ref={ref} className='relative m-auto  h-[80vh] w-[80%] rounded-lg md:w-[38%] lg:h-[80%]'>
+                <div
+                    ref={ref}
+                    className='relative m-auto  h-[80vh] w-[80%] rounded-lg md:w-[38%] lg:h-[80%] lg:w-[60%]'
+                >
                     <img
                         key='1'
                         className='h-full w-full rounded-lg object-fill lg:h-[100%]'
