bdc7bf9feaef9ed7a096fc9b6db92e99529a195e
Dan Jecu <jdanalexandru@gmail.com>
2023-05-21 06:58:51 +0200

    added cinemania

--- components/projects/Cinemania.tsx
+++ components/projects/Cinemania.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import { useRef } from 'react';
 import { useInView } from 'framer-motion';
 
-export const Project: React.FC = () => {
+export const Cinemania: React.FC = () => {
     const ref = useRef(null);
     const isInView = useInView(ref, { once: true });
 
@@ -14,42 +14,10 @@ export const Project: React.FC = () => {
     };
 
     return (
-        <section
-            className='m-auto flex w-[95vw] flex-col lg:grid lg:grid-cols-2 lg:grid-rows-[1fr_5fr] '
-            style={style}
-        >
-            <span className='flex w-[95%] items-center justify-center p-7 text-white lg:col-start-1 lg:col-end-3 lg:mt-auto lg:w-[50%]  lg:pt-24'>
-                <h2 className='pr-4 text-2xl font-bold '>Projects</h2>
-                <div className='h-[1.5px] flex-1 bg-white'></div>
-            </span>
-            <div className='flex'>
-                <div className='relative m-auto hidden h-[80vh] w-[80%] rounded-lg md:w-[38%] lg:h-[80%] lg:w-[42%] xl:block'>
-                    <img
-                        key='1'
-                        className='h-full w-full rounded-lg object-fill lg:h-[100%] '
-                        src='./projects/mealShare2.png'
-                        alt='profile picture'
-                        width=''
-                        height=''
-                    />
-                </div>
-                <div
-                    ref={ref}
-                    className='relative m-auto h-[80vh] w-[80%] rounded-lg md:w-[38%] lg:h-[80%] lg:w-[42%]'
-                >
-                    <img
-                        key='1'
-                        className='h-full w-full rounded-lg object-fill lg:h-[100%]'
-                        src='./projects/mealShare1.png'
-                        alt='profile picture'
-                        width=''
-                        height=''
-                    />
-                </div>
-            </div>
+        <>
             <div className='lg:place-self-center'>
                 <h2 className='pb-2 pt-4 text-center text-xl  text-zinc-200 lg:pb-4 lg:text-2xl'>
-                    Meal Sharing
+                    Cinemania
                 </h2>
                 <div className='flex flex-wrap justify-center gap-3 lg:mx-auto lg:w-9/12'>
                     <span className='flex items-center gap-1 rounded-xl bg-card p-[.35rem] text-black'>
@@ -63,21 +31,21 @@ export const Project: React.FC = () => {
                     </span>
                     <span className='flex items-center gap-1 rounded-xl bg-card p-[.30rem] text-black'>
                         <Image
-                            src='/icons/frontend/TypeScript.svg'
+                            src='/icons/frontend/MUI.svg'
                             width='18'
                             height='18'
-                            alt='typescript icon'
+                            alt='mui icon'
                         />
-                        <h5 className='text-xs text-secondary'>Typescript</h5>
+                        <h5 className='text-xs text-secondary'>MUI</h5>
                     </span>
                     <span className='flex items-center gap-1 rounded-xl bg-card p-[.35rem] text-black'>
                         <Image
-                            src='/icons/frontend/CSS.svg'
+                            src='/icons/frontend/Emotion-Dark.svg'
                             width='18'
                             height='18'
-                            alt='css'
+                            alt='emotion'
                         />
-                        <h5 className='text-xs text-secondary'>CSS Modules</h5>
+                        <h5 className='text-xs text-secondary'>Emotion</h5>
                     </span>
                     <span className='flex items-center gap-1 rounded-xl bg-card p-[.35rem] text-black'>
                         <Image
@@ -99,38 +67,59 @@ export const Project: React.FC = () => {
                     </span>
                     <span className='flex items-center gap-1 rounded-xl bg-card p-[.35rem] text-black'>
                         <Image
-                            src='/icons/backend/PostgreSQL.svg'
+                            src='/icons/backend/MySQL.svg'
                             width='18'
                             height='18'
-                            alt='postgresql'
+                            alt='mysql'
                         />
-                        <h5 className='text-xs text-secondary'>PostgreSQL</h5>
+                        <h5 className='text-xs text-secondary'>MySQL</h5>
                     </span>
                 </div>
 
                 <p className='m-auto w-11/12 pt-6 text-center text-sm font-light leading-5 tracking-wide text-secondary lg:text-[1.08rem] lg:leading-7'>
-                    Fully featured meal booking system. Developed with React
-                    coupled with Node/Express and PostgreSQL on the backend.
+                     Cinemania is a dynamic movie app developed using React, Material-UI (MUI), Emotion, and Express.js. This app is designed to be your go-to companion for all things movies, providing a seamless and immersive experience for movie enthusiasts.
                 </p>
                 <div className='[&>*:font-bold] flex justify-evenly py-6 text-[1rem] text-white'>
                     <button className='button'>
                         <a
                             target='_blank'
-                            href='https://github.com/danJecu/meal-sharing-app'
+                            href='https://github.com/HackYourFuture-CPH/CINEMANIA'
                         >
                             SOURCE
                         </a>
                     </button>
-                    <button className='button'>
-                        <a
-                            target='_blank'
-                            href='https://meal-sharing-app.vercel.app/'
-                        >
+                    <button className='button' disabled>
+                        <a target='_blank' href=''>
                             LIVE
                         </a>
                     </button>
                 </div>
             </div>
-        </section>
+            <div className='flex' style={style}>
+                <div className='relative m-auto hidden h-[80vh] w-[80%] rounded-lg md:w-[38%] lg:h-[80%] lg:w-[42%] xl:block'>
+                    <img
+                        key='1'
+                        className='h-full w-full rounded-lg object-fill lg:h-[100%] '
+                        src='./projects/cinemania2.png'
+                        alt='profile picture'
+                        width=''
+                        height=''
+                    />
+                </div>
+                <div
+                    ref={ref}
+                    className='relative m-auto h-[80vh] w-[80%] rounded-lg md:w-[38%] lg:h-[80%] lg:w-[42%]'
+                >
+                    <img
+                        key='1'
+                        className='h-full w-full rounded-lg object-fill lg:h-[100%]'
+                        src='./projects/cinemania1.png'
+                        alt='profile picture'
+                        width=''
+                        height=''
+                    />
+                </div>
+            </div>
+        </>
     );
 };
--- components/sections/ProjectsSection.tsx
+++ components/sections/ProjectsSection.tsx
@@ -1,137 +1,15 @@
-import Image from 'next/image';
-import { useRef } from 'react';
+import { Cinemania } from '../projects/Cinemania';
+import { MealApp } from '../projects/MealApp';
 
-import { useInView } from 'framer-motion';
-
-/* eslint-disable @next/next/no-img-element */
 export const ProjectsSection: React.FC = () => {
-    const ref = useRef(null);
-    const isInView = useInView(ref, { once: true });
-
-    const style = {
-        transform: isInView ? 'none' : 'translateX(200px)',
-        opacity: isInView ? 1 : 0,
-        transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
-    };
-
     return (
-        <section
-            className='m-auto flex w-[95vw] flex-col lg:grid lg:grid-cols-2 lg:grid-rows-[1fr_5fr] '
-            style={style}
-        >
+        <section className='m-auto flex w-[95vw] flex-col lg:grid lg:grid-cols-2 lg:grid-rows-[1fr_5fr_5fr] '>
             <span className='flex w-[95%] items-center justify-center p-7 text-white lg:col-start-1 lg:col-end-3 lg:mt-auto lg:w-[50%]  lg:pt-24'>
                 <h2 className='pr-4 text-2xl font-bold '>Projects</h2>
                 <div className='h-[1.5px] flex-1 bg-white'></div>
             </span>
-            <div className='flex'>
-                <div className='relative m-auto hidden h-[80vh] w-[80%] rounded-lg md:w-[38%] lg:h-[80%] lg:w-[42%] xl:block'>
-                    <img
-                        key='1'
-                        className='h-full w-full rounded-lg object-fill lg:h-[100%] '
-                        src='./projects/mealShare2.png'
-                        alt='profile picture'
-                        width=''
-                        height=''
-                    />
-                </div>
-                <div
-                    ref={ref}
-                    className='relative m-auto h-[80vh] w-[80%] rounded-lg md:w-[38%] lg:h-[80%] lg:w-[42%]'
-                >
-                    <img
-                        key='1'
-                        className='h-full w-full rounded-lg object-fill lg:h-[100%]'
-                        src='./projects/mealShare1.png'
-                        alt='profile picture'
-                        width=''
-                        height=''
-                    />
-                </div>
-            </div>
-            <div className='lg:place-self-center'>
-                <h2 className='pb-2 pt-4 text-center text-xl  text-zinc-200 lg:pb-4 lg:text-2xl'>
-                    Meal Sharing
-                </h2>
-                <div className='flex flex-wrap justify-center gap-3 lg:mx-auto lg:w-9/12'>
-                    <span className='flex items-center gap-1 rounded-xl bg-card p-[.35rem] text-black'>
-                        <Image
-                            src='/icons/frontend/React-Dark.svg'
-                            width='18'
-                            height='18'
-                            alt='react'
-                        />
-                        <h5 className='text-xs text-secondary'>React</h5>
-                    </span>
-                    <span className='flex items-center gap-1 rounded-xl bg-card p-[.30rem] text-black'>
-                        <Image
-                            src='/icons/frontend/TypeScript.svg'
-                            width='18'
-                            height='18'
-                            alt='typescript icon'
-                        />
-                        <h5 className='text-xs text-secondary'>Typescript</h5>
-                    </span>
-                    <span className='flex items-center gap-1 rounded-xl bg-card p-[.35rem] text-black'>
-                        <Image
-                            src='/icons/frontend/CSS.svg'
-                            width='18'
-                            height='18'
-                            alt='css'
-                        />
-                        <h5 className='text-xs text-secondary'>CSS Modules</h5>
-                    </span>
-                    <span className='flex items-center gap-1 rounded-xl bg-card p-[.35rem] text-black'>
-                        <Image
-                            src='/icons/backend/ExpressJS.svg'
-                            width='18'
-                            height='18'
-                            alt='expressjs icon'
-                        />
-                        <h5 className='text-xs text-secondary'>ExpressJS</h5>
-                    </span>
-                    <span className='flex items-center gap-1 rounded-xl bg-card p-[.35rem] text-black'>
-                        <Image
-                            src='/icons/backend/KnexJS.png'
-                            width='18'
-                            height='18'
-                            alt='knexjs'
-                        />
-                        <h5 className='text-xs text-secondary'>KnexJS</h5>
-                    </span>
-                    <span className='flex items-center gap-1 rounded-xl bg-card p-[.35rem] text-black'>
-                        <Image
-                            src='/icons/backend/PostgreSQL.svg'
-                            width='18'
-                            height='18'
-                            alt='postgresql'
-                        />
-                        <h5 className='text-xs text-secondary'>PostgreSQL</h5>
-                    </span>
-                </div>
-
-                <p className='m-auto w-11/12 pt-6 text-center text-sm font-light leading-5 tracking-wide text-secondary lg:text-[1.08rem] lg:leading-7'>
-                    Fully featured meal booking system. Developed with React
-                    coupled with Node/Express and PostgreSQL on the backend.
-                </p>
-                <div className='[&>*:font-bold] flex justify-evenly py-6 text-[1rem] text-white'>
-                    <button className='button'>
-                        <a
-                            target='_blank'
-                            href='https://github.com/danJecu/meal-sharing-app'
-                        >
-                            SOURCE
-                        </a>
-                    </button>
-                    <button className='button'>
-                        <a
-                            target='_blank'
-                            href='https://meal-sharing-app.vercel.app/'
-                        >
-                            LIVE
-                        </a>
-                    </button>
-                </div>
-            </div>
+            <MealApp />
+            <Cinemania />
         </section>
     );
 };
