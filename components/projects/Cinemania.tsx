728b15e90c9f11035ad374b56711f2cb0101c5cd
Dan Jecu <60711891+danJecu@users.noreply.github.com>
2023-05-26 10:55:16 +0000

    changed sizes of projects

--- components/projects/Cinemania.tsx
+++ components/projects/Cinemania.tsx
@@ -101,7 +101,7 @@ export const Cinemania: React.FC = () => {
                 className='flex items-center justify-around gap-10 px-1 lg:w-[50vw]'
                 style={style}
             >
-                <div className='xl-max-w-[48%] relative hidden h-[80vh] w-[80%] rounded-lg md:w-[38%] lg:h-[75.3%] xl:block xl:min-w-[44%] '>
+                <div className='xl-max-w-[48%] relative mx-auto my-auto h-[80vh] max-w-[80vw] rounded-lg md:max-w-[50%] lg:h-[75.3%] lg:max-w-[70%] xl:min-w-[43%] hidden'>
                     <img
                         key='1'
                         className='h-full w-full rounded-lg object-fill lg:h-[100%] '
@@ -110,7 +110,7 @@ export const Cinemania: React.FC = () => {
                         width=''
                         height=''
                     />
-                    {/* // TODO FIX THE WIDTH */}
+              
                 </div>
                 <div
                     ref={ref}
