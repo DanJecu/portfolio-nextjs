1b9d9f5b9f1597e08aad467b7e9a2191f6447d1d
dj-at-dot-codezo <dj@codezo.dk>
2023-11-26 17:08:08 +0100

    upgrade to next-14

---
+++ app/components/Logo.tsx
@@ -0,0 +1,100 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+export const Logo: React.FC = () => {
+  const [showText, setShowText] = useState(false);
+  useEffect(() => {
+    if (!showText) setShowText(true);
+  }, [showText]);
+  return (
+    <div>
+      <h1 className="pb-1 text-center text-4xl md:text-5xl lg:text-6xl xl:text-7xl">
+        <motion.span
+          layout
+          key="d"
+          style={{ display: "inline-block" }}
+          transition={{ ease: "linear", duration: 0.7, delay: 1 }}
+        >
+          D
+        </motion.span>
+        {showText && (
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            style={{ display: "inline-block" }}
+            transition={{ delay: 2, ease: "easeIn", duration: 2 }}
+          >
+            AN
+          </motion.span>
+        )}
+        {showText && <span> </span>}
+        <motion.span
+          layout
+          key="j"
+          style={{ display: "inline-block" }}
+          transition={{ ease: "linear", duration: 0.7, delay: 1 }}
+        >
+          J
+        </motion.span>
+        {showText && (
+          <motion.span
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            style={{ display: "inline-block" }}
+            transition={{ delay: 2, ease: "easeIn", duration: 2 }}
+          >
+            ECU
+          </motion.span>
+        )}
+      </h1>
+      <motion.h2
+        className="pt-1 text-center text-2xl font-light text-[#a9aec0] md:text-3xl lg:text-4xl xl:text-5xl"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 3, ease: "easeOut", duration: 0.7 }}
+      >
+        Full Stack Developer
+      </motion.h2>
+      <motion.div
+        className="flex justify-evenly pt-10"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 3.7, ease: "easeOut", duration: 0.9 }}
+      >
+        <a href="https://github.com/danJecu" target="_blank">
+          <Image
+            height="50"
+            width="50"
+            src="./icons/misc/githubMono.svg"
+            alt="github"
+            className="invert transition duration-300 ease-out hover:scale-125"
+          />
+        </a>
+        {/*    <a href='https://github.com/danJecu' target='_blank'>
+                    <Image
+                        height='40'
+                        width='40'
+                        src='/icons/misc/cv.svg'
+                        alt='cv'
+                        className='transition duration-300 ease-out invert hover:scale-125'
+                    />
+                </a> */}
+        <a
+          href="https://www.linkedin.com/in/dan-alexandru-jecu/"
+          target="_blank"
+        >
+          <Image
+            height="50"
+            width="50"
+            src="/icons/misc/linkedinMono.svg"
+            alt="linkedin"
+            className="invert transition duration-300 ease-out hover:scale-125"
+          />
+        </a>
+      </motion.div>
+    </div>
+  );
+};
---
+++ app/sections/HeroSection.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Logo } from "../components/Logo";
+
+export const HeroSection: React.FC = () => {
+  return (
+    <section className="custom-bg m-auto flex w-[100%] flex-col items-center justify-center ">
+      <div className="my-auto flex font-bold text-white ">
+        <Logo />
+      </div>
+      <Image
+        alt="down-arrow"
+        src="/down-arrow.png"
+        width="50"
+        height="50"
+        className="mx-auto animate-bounce self-end pb-12 opacity-60 invert filter"
+      />
+    </section>
+  );
+};
